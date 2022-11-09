a0bb8c45774de0282691e2895308f725acdb9d44
tourssliver <theobaldnartha@gmail.com>
2022-11-09 21:54:59 +0800

    feat: add bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import bg from '/public/images/bg.webp'
 
 export default function Home() {
   return (
-    <div className="dark:bg-[#050506] w-full h-full">
-      <div>123</div>
+    <div className="bg-[#050506] dark:bg-[#050506] w-full m-h-full">
+      <Image src={bg} alt='bg' className='brightness-50'></Image>
     </div>
   )
 }
