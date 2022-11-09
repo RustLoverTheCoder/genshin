9a4e060581742266251b4feb82686c427232d332
maxiang <maxiang@haoshuo.com>
2022-11-09 18:19:28 +0800

    feat: add dark page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="bg-[#050506]">
+    <div className="dark:bg-[#050506] w-full h-full">
       <div>123</div>
     </div>
-  );
+  )
 }
