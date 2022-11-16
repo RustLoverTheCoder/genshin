a959dbc99d6b634be349d723dd6a33342b624d08
maxiang <maxiang@haoshuo.com>
2022-11-16 10:59:16 +0800

    feat: add ahooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import bg from '/public/images/bg.webp'
 
 export default function Home() {
   return (
-    <div className="h-auto w-full bg-[#050506] dark:bg-[#050506]" style={{ minHeight: '100%' }}>
-      <Image src={bg} alt="bg" className="brightness-50"></Image>
+    <div className="h-auto w-full bg-white dark:bg-[#050506]" style={{ minHeight: '100%' }}>
+      <Image src={bg} alt="bg"></Image>
     </div>
   )
 }
